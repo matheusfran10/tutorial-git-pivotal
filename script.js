6471187e625f7a3507eab56c9c5d51c2d1e0c59a
Matheus Francisco <mateusfrancisco1324@gmail.com>
2022-11-25 20:40:41 -0300

    Gatos aleatorios

---
+++ script.js
@@ -0,0 +1,29 @@
+/*link de apoio para efetuar o loop de gatinhos*/
+const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
+const catBtn = document.getElementById('change-cat');
+
+/*configuração do javascript para efetuar a troca de imagens */
+const getCats = async () => {
+   const catsImgDiv = document.querySelector(".catsImgDiv")
+   catsImgDiv.innerHTML = ""
+   const data = await fetch(BASE_URL)
+      .then(res => res.json())
+      .then((data) => {
+         let catsImgUrl = data[0].url
+
+         let catsImgEl = document.createElement("img")
+         catsImgEl.setAttribute('src', `${catsImgUrl}`)
+
+         let catsImgDiv = document.querySelector(".catsImgDiv")
+         catsImgDiv.appendChild(catsImgEl)
+      })
+
+      .catch(cat => console.log(cat))
+
+}
+const loadImg = async () => {
+   const catImg = document.getElementById('cat');
+   catImg.src = await getCats();
+}
+button.addEventListener('click', loadImg);
+loadImg();
